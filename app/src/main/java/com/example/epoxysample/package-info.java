9e99288c436f69ffbf27965cb0693eb7e2de32b3
Daishin16 <sakuya1202@icloud.com>
2020-05-11 00:58:02 +0900

    fix error / reason ->package-info.java:1 ; <-✖︎

--- app/src/main/java/com/example/epoxysample/package-info.java
+++ app/src/main/java/com/example/epoxysample/package-info.java
@@ -1,4 +1,5 @@
-@EpoxyDataBindingPattern(rClass = R.class, layoutPrefix = "item_");
+@EpoxyDataBindingPattern(rClass = R.class, layoutPrefix = "item_")
+
 package com.example.epoxysample;
 
 import com.airbnb.epoxy.EpoxyDataBindingPattern;
